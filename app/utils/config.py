1560c88d82fed9550fb36169aca2554ab1cdf588
Manibala Sinha <manibalak@gmail.com>
2026-04-08 09:48:30 -0400

    Add environment variables and enable debug mode
    Added environment variables for tracing and project configuration, and enabled debug mode.

--- app/utils/config.py
+++ app/utils/config.py
@@ -2,6 +2,11 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 import os
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2", "false")
+LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT", "default")
+
+# debug flag
+DEBUG = True
 
 MODEL_NAME = "gpt-4o-mini"
 
