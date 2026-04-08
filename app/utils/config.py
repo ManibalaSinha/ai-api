203cb1ee80f1460ee05966adaf4f4ff3e764f0e2
Manibala Sinha <manibalak@gmail.com>
2026-04-08 09:25:28 -0400

    Configure LangChain LLM and OpenAI embeddings
    Added LangChain LLM and OpenAI embeddings configuration.

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,7 +1,16 @@
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 import os
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 MODEL_NAME = "gpt-4o-mini"
 
-TOP_K = 3
+# LangChain LLM
+llm = ChatOpenAI(
+    api_key=OPENAI_API_KEY,
+    model=MODEL_NAME,
+    temperature=0
+)
+
+# Embeddings
+embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
